498b6298bf06ba6291063b945dfac23f34081d25
joknarf <joknarf@free.fr>
2018-11-29 14:35:20 +0100

    add table user

--- app/models.py
+++ app/models.py
@@ -6,6 +6,14 @@ class Tweet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(280))
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
         return f"<Tweet #{self.id}>"
+
+class User(db.Model):
+    __table__name = "user"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(280))
+    api_token = db.Column(db.String(280))
+    children = db.relationship("Tweet")
